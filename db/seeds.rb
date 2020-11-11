d011674d1fbb347f5d497dab4cd60be6fe53229d
aida tagmouti <aida.tagmouti@gmail.com>
2020-11-11 17:07:09 +0100

    routes db and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'View Flat Paris',
+  address: '1 rue de la paix, Paris',
+  description: 'An amazing flat in Paris with a view on Eiffel Tower',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Nice Flat Madrid',
+  address: '1 tapas street Madrid',
+  description: 'A lovely summer feel for this spacious garden flat.',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: ' Burritos Flat Mexico',
+  address: '167 mexico street Mexico',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 50,
+  number_of_guests: 6
+)
